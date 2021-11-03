4a73e0a32af26a287a66be7aac72b62129ca8817
Matthew Dowell <mdowell723@gmail.com>
2021-11-03 17:02:39 -0600

    added more questions button and sorted list of questions by helpfulness

--- client/src/components/questions/Question.jsx
+++ client/src/components/questions/Question.jsx
@@ -4,6 +4,7 @@ const Question = (props) => {
   return (
     <>
       <p>Q:  {props.question.question_body}</p>
+      {Object.values(props.question.answers).map(answer => <p key={answer.id}>A:  {answer.body}</p>).slice(0, 2)}
     </>
   )
 }
--- client/src/components/questions/QuestionsList.jsx
+++ client/src/components/questions/QuestionsList.jsx
@@ -3,16 +3,25 @@ import { qaContext } from '../../context/qaContext.js';
 import Question from './Question.jsx';
 
 const QuestionsList = () => {
-  //const { currentQuestions } = useContext(qaContext);
+  const currentQuestions = useContext(qaContext);
+  const [numberOfQsDisplayed, setNumberOfQsDisplayed] = useState(4)
+
+  function HandleMoreQsClick() {
+    setNumberOfQsDisplayed(currentQuestions.length);
+  }
 
   return (
     <div>
-      <div>Questions List Component</div>
       <qaContext.Consumer>
         {(questions) => (
-          questions.map(question => <Question question={question}/>)
+          questions
+            .map((question, index) => 
+              <Question key={index} question={question}/>)
+            .slice(0, numberOfQsDisplayed)
+            .sort((a, b) => a.question_helpfulness - b.question_helpfulness)
         )}
       </qaContext.Consumer>
+      <button onClick={HandleMoreQsClick}>More Answered Questions</button>
     </div>
   )
 }
