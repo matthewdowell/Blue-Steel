b9b9ae501659eec4165451c34c40319d639fd51d
John Duval <john@rockitscienceagency.com>
2021-11-03 17:15:02 -0600

    added css styling and broke out components for product overview

---
+++ client/src/components/product_overview/subcomponents/ImageGallery.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const ImageGallery = () => {
+
+
+    return (
+        <div style={{ border: 'none', background: 'lightgray', display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexBasis: '65%' }}>
+            <h1>Image Gallery</h1>
+        </div>
+    )
+
+}
+
+export default ImageGallery;
