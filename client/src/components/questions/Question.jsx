9489c7ba9e9b90173771c975891d7b7865668bb4
Matthew Dowell <mdowell723@gmail.com>
2021-11-03 21:32:50 -0600

    added user, date, and some styling to question component

--- client/src/components/questions/Question.jsx
+++ client/src/components/questions/Question.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 const Question = (props) => {
   return (
     <>
-      <p>Q:  {props.question.question_body}</p>
+      <p><b>Q:  {props.question.question_body}</b></p>
       <div style={{display: 'flex'}}>
-        <p style={{paddingRight: '5px'}}>A:  </p>
+        <p style={{paddingRight: '5px'}}><b>A:  </b></p>
         <div>{Object
           .values(props.question.answers)
-          .map(answer => <p key={answer.id}>{answer.body}</p>)
+          .map(answer => 
+            <><p key={answer.id}>{answer.body}</p>
+            {answer.answerer_name === 'Seller'
+              ? <div>
+                  <div>
+                    {`by `}<b>{answer.answerer_name}</b>
+                    {`, ${new Date(answer.date).toDateString().slice(4)}`}
+                  </div>
+                </div>
+              : <div>{`by ${answer.answerer_name}, ${new Date(answer.date).toDateString().slice(4)}`}</div>
+            }</>)
           .slice(0, 2)}
         </div>
       </div>
