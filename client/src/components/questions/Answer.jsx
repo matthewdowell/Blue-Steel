c67b85c30a884070262beb4c430aabd24ee3b52c
Matthew Dowell <mdowell723@gmail.com>
2021-11-04 09:59:24 -0600

    succcessful refactor of answer component

--- client/src/components/questions/Answer.jsx
+++ client/src/components/questions/Answer.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 
-const Answer = (props) => {
+const Answer = ({ answer }) => {
+
+  // const[helpfulVotes, setHelpfulVotes] = useState(props.question)
+
+  // function handleYesClick() {
+
+  // }
 
   return (
-    
+    <><p>{answer.body}</p>
+            {answer.answerer_name === 'Seller'
+              ? <div style={{display: 'flex'}}>
+                  <div>{`by `}<b>{answer.answerer_name}</b>{`, ${new Date(answer.date).toDateString().slice(4)}`}</div>
+                  <div style={{padding: '0px 10px'}}>Helpful? </div>
+                  <div style={{textDecoration: 'underline', paddingRight: '5px'}}>Yes</div>
+                  <div>{`(${answer.helpfulness})`}</div>
+                </div>
+              : <div style={{display: 'flex'}}>
+                  <div>{`by ${answer.answerer_name}, ${new Date(answer.date).toDateString().slice(4)}`}</div>
+                  <div style={{padding: '0px 10px'}}>Helpful? </div>
+                  <div 
+                    style={{textDecoration: 'underline', paddingRight: '5px'}}
+                    // onClick={{handleYesClick}}
+                  >Yes</div>
+                  <div>{`(${answer.helpfulness})`}</div>
+                </div>
+            }
+  </>
   )
 }
+
+export default Answer;
--- client/src/components/questions/Question.jsx
+++ client/src/components/questions/Question.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Answer from './Answer.jsx';
 
 const Question = (props) => {
 
@@ -15,25 +16,7 @@ const Question = (props) => {
         <p style={{paddingRight: '5px'}}><b>A:  </b></p>
         <div>{Object
           .values(props.question.answers)
-          .map(answer => 
-            <><p key={answer.id}>{answer.body}</p>
-            {answer.answerer_name === 'Seller'
-              ? <div style={{display: 'flex'}}>
-                  <div>{`by `}<b>{answer.answerer_name}</b>{`, ${new Date(answer.date).toDateString().slice(4)}`}</div>
-                  <div style={{padding: '0px 10px'}}>Helpful? </div>
-                  <div style={{textDecoration: 'underline', paddingRight: '5px'}}>Yes</div>
-                  <div>{`(${answer.helpfulness})`}</div>
-                </div>
-              : <div style={{display: 'flex'}}>
-                  <div>{`by ${answer.answerer_name}, ${new Date(answer.date).toDateString().slice(4)}`}</div>
-                  <div style={{padding: '0px 10px'}}>Helpful? </div>
-                  <div 
-                    style={{textDecoration: 'underline', paddingRight: '5px'}}
-                    // onClick={{handleYesClick}}
-                  >Yes</div>
-                  <div>{`(${answer.helpfulness})`}</div>
-                </div>
-            }</>)
+          .map(answer => <Answer answer={answer} key={answer.id}/>)
           .slice(0, 2)}
         </div>
       </div>
