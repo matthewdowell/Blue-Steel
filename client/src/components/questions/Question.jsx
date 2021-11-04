42c7055beb076cce018bcbca8dded31b28094227
Matthew Dowell <mdowell723@gmail.com>
2021-11-03 19:42:59 -0600

    finished list component ticket and working on question component

--- client/src/components/questions/Question.jsx
+++ client/src/components/questions/Question.jsx
@@ -4,7 +4,14 @@ const Question = (props) => {
   return (
     <>
       <p>Q:  {props.question.question_body}</p>
-      {Object.values(props.question.answers).map(answer => <p key={answer.id}>A:  {answer.body}</p>).slice(0, 2)}
+      <div style={{display: 'flex'}}>
+        <p style={{paddingRight: '5px'}}>A:  </p>
+        <div>{Object
+          .values(props.question.answers)
+          .map(answer => <p key={answer.id}>{answer.body}</p>)
+          .slice(0, 2)}
+        </div>
+      </div>
     </>
   )
 }
