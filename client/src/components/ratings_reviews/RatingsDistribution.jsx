d78266831408fd5535bd88d3ca0476dbc1a45d62
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-04 18:09:53 -0500

    Added progress bar and review submission form

--- client/src/components/ratings_reviews/RatingsDistribution.jsx
+++ client/src/components/ratings_reviews/RatingsDistribution.jsx
@@ -1,15 +1,57 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/prop-types */
 import React from 'react';
+import ratingsReviewsHelpers from './ratingsReviewsHelpers';
 
 const RatingsDistribution = (props) => {
-  var reviews = props.reviews;
-
+  const { reviews } = props;
   return (
-    <div className="ratingsDistribution">
-      <div>5 stars</div>
-      <div>4 stars</div>
-      <div>3 stars</div>
-      <div>2 stars</div>
-      <div>1 stars</div>
+    <div className="ratingDistribution">
+      <div className="ratingBarContainer">
+        <span className="ratingCount">5 stars</span>
+        <progress
+          className="ratingBar"
+          value={ratingsReviewsHelpers.countReviewsWithRating(reviews, 5) / reviews.length}
+        >
+        </progress>
+        {ratingsReviewsHelpers.countReviewsWithRating(reviews, 5)}
+      </div>
+      <div className="ratingBarContainer">
+        <span className="ratingCount">4 stars</span>
+        <progress
+          className="ratingBar"
+          value={ratingsReviewsHelpers.countReviewsWithRating(reviews, 4) / reviews.length}
+        >
+        </progress>
+        {ratingsReviewsHelpers.countReviewsWithRating(reviews, 4)}
+      </div>
+      <div className="ratingBarContainer">
+        <span className="ratingCount">3 stars</span>
+        <progress
+          className="ratingBar"
+          value={ratingsReviewsHelpers.countReviewsWithRating(reviews, 3) / reviews.length}
+        >
+        </progress>
+        {ratingsReviewsHelpers.countReviewsWithRating(reviews, 3)}
+      </div>
+      <div className="ratingBarContainer">
+        <span className="ratingCount">2 stars</span>
+        <progress
+          className="ratingBar"
+          value={ratingsReviewsHelpers.countReviewsWithRating(reviews, 2) / reviews.length}
+        >
+        </progress>
+        {ratingsReviewsHelpers.countReviewsWithRating(reviews, 2)}
+      </div>
+      <div className="ratingBarContainer">
+        <span className="ratingCount">1 stars</span>
+        <progress
+          className="ratingBar"
+          value={ratingsReviewsHelpers.countReviewsWithRating(reviews, 1) / reviews.length}
+        >
+        </progress>
+        {ratingsReviewsHelpers.countReviewsWithRating(reviews, 1)}
+      </div>
     </div>
   );
 };
