9ba4d4ec7bfdbe3f4ce72c676152d14e63263836
Matthew Dowell <mdowell723@gmail.com>
2021-11-04 08:54:44 -0600

    Question component is working and about to refactor to an answer component

--- client/src/components/questions/Question.jsx
+++ client/src/components/questions/Question.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Question = (props) => {
+
+  // const[helpfulVotes, setHelpfulVotes] = useState(props.question)
+
+  // function handleYesClick() {
+
+  // }
+
   return (
     <>
       <p><b>Q:  {props.question.question_body}</b></p>
@@ -17,7 +24,15 @@ const Question = (props) => {
                   <div style={{textDecoration: 'underline', paddingRight: '5px'}}>Yes</div>
                   <div>{`(${answer.helpfulness})`}</div>
                 </div>
-              : <div>{`by ${answer.answerer_name}, ${new Date(answer.date).toDateString().slice(4)}`}</div>
+              : <div style={{display: 'flex'}}>
+                  <div>{`by ${answer.answerer_name}, ${new Date(answer.date).toDateString().slice(4)}`}</div>
+                  <div style={{padding: '0px 10px'}}>Helpful? </div>
+                  <div 
+                    style={{textDecoration: 'underline', paddingRight: '5px'}}
+                    // onClick={{handleYesClick}}
+                  >Yes</div>
+                  <div>{`(${answer.helpfulness})`}</div>
+                </div>
             }</>)
           .slice(0, 2)}
         </div>
