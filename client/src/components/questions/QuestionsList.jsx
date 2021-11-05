2e3ea62f1d49d4109135ce03737d10adb534baed
Matthew Dowell <mdowell723@gmail.com>
2021-11-04 20:48:41 -0600

    made progress on get more answers button

--- client/src/components/questions/QuestionsList.jsx
+++ client/src/components/questions/QuestionsList.jsx
@@ -4,10 +4,12 @@ import Question from './Question.jsx';
 
 const QuestionsList = () => {
   const currentQuestions = useContext(qaContext);
-  const [numberOfQsDisplayed, setNumberOfQsDisplayed] = useState(4)
+  const [numberOfQsDisplayed, setNumberOfQsDisplayed] = useState(2)
+  const [moreQsButtonDisplayed, setMoreQsButtonDisplayed] = useState(true)
 
   function HandleMoreQsClick() {
     setNumberOfQsDisplayed(currentQuestions.length);
+    setMoreQsButtonDisplayed(false);
   }
 
   return (
@@ -22,7 +24,11 @@ const QuestionsList = () => {
               .slice(0, numberOfQsDisplayed)
               .sort((a, b) => b.props.helpfulness - a.props.helpfulness)}
              </div>
-             <button onClick={HandleMoreQsClick}>More Answered Questions</button></>
+             {currentQuestions.length > 2 && moreQsButtonDisplayed
+             ? <button onClick={HandleMoreQsClick}>More Answered Questions</button>
+             : null
+             }
+            </>
         )}
       </qaContext.Consumer>
     </div>
