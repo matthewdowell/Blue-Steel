018c8e915da8ab2a120537e44075628e39cdf2ed
Matthew Dowell <mdowell723@gmail.com>
2021-11-05 17:40:18 -0600

    made progress on question modal

--- client/src/components/questions/QuestionForm.jsx
+++ client/src/components/questions/QuestionForm.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDom from 'react-dom';
+import { ProductContext } from '../../context/globalContext';
 
 const QuestionForm = () => {
+  const { currentProduct } = useContext(ProductContext);
+
   return (
     <div 
       id='questionModal' 
       style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
         height: '600px',
         width: '400px',
         border: "2px solid black",
         backgroundColor: 'white',
       }}>
-      Question Form
+      <h3 style={{marginBottom: '0px'}}>Ask Your Question</h3>
+      <div style={{marginTop: '5px'}}>About the {currentProduct.name}</div>
     </div>
   )
 }
