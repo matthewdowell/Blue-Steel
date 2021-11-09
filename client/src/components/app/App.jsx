129ef437491bdc32063e8c3479fd17ada6cd4b20
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-08 20:30:54 -0600

    Added CSS

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
     created_at: '2021-08-13T14:40:29.181Z',
     default_price: '140.00',
     description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest, surroundings.',
-    id: 44390,
+    id: 44388,
     name: 'Camo Onesie',
     slogan: 'Blend in to your crowd',
     updated_at: '2021-08-13T14:40:29.181Z'
@@ -37,9 +37,7 @@ const App = () => {
       {/* eslint-disable-next-line object-curly-newline */}
       <ProductContext.Provider value={{ currentProduct, setCurrentProduct, products, setProducts }}>
         {/* <div><RelatedProductsList /></div> */}
-        {/* <div><QuestionsAnswers /></div>
-        <div><RatingsReviews /></div> */}
-        {/* <div><ProductOverview /></div> */}
+        <div><ProductOverview /></div>
         {/* <div><RelatedItems /></div> */}
         {/* <div><QuestionsAnswers /></div>
         <div><RatingsReviews /></div> */}
